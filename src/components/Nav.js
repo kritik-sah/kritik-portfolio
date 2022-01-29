d6420a6676029d7941d77ab78b6da0af975a630b
kritik sah <kritiksah0@gmail.com>
2022-01-30 03:25:48 +0530

    portfolio update 2022

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ const Nav = () => {
 
   <div className="container">
     <a className="navbar-brand" href="/">
-      {/* <img src={Logo} alt="" width="30" height="30" />*/}<span>🟢READY FOR WORK</span> 
+      {/* <img src={Logo} alt="" width="30" height="30" />*/}<span>&#128994;READY FOR WORK</span> 
     </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
